d92316880f8362092fcf27bb87bd95248967ff99
diph-Ho <77926543+diph-Ho@users.noreply.github.com>
2021-02-07 15:59:44 +0900

    member save with memeory

--- startpack/dpjava/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ startpack/dpjava/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -10,13 +10,9 @@ import java.util.List;
 import java.util.Optional;
 
 
-@Service //이걸 선언하기 이전에 MemberService는 스프링 컨테이너에 등록되지 않으므로 선언해야 컨트롤러에서 객체를 선언할때 인식이 가능하다.
 public class MemberService {
     private final MemberRepository memberRepository;
 
-    @Autowired // 스프링 컨테이너에 있는 MemberRepositroy와 MemberService를 연결시켜준다
-    //Dependency inejection(의존성 주입)
-    //이때 Repository 어노테이션은 MemoryMemberRepository에 있으므로 해당하는 것을 연결시켜준다.
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
