5f0e105f1cb322b5aa25c09f9489ad4491b0afa7
diph-Ho <77926543+diph-Ho@users.noreply.github.com>
2021-02-10 18:24:31 +0900

    Integration Test

---
+++ startpack/dpjava/hello-spring/src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -0,0 +1,67 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@Transactional // Transactional 을 Test코드에서 어노테이션하면 실질적으로 DB에 데이터가 들어가지 않고 테스트 된다.
+class MemberServiceIntegrationTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    void 회원가입() {
+        //given 이런 상황이 주어졌을때
+        Member member = new Member();
+        member.setName("hokyun");
+
+
+        //when 이렇게 실행하면
+        Long saveId = memberService.join(member);
+
+        //then 이런 결과가 나와야 한다.
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+
+    }
+
+    @Test
+    public void 중복_회원_예외() {
+        //given
+        Member member1 = new Member();
+        member1.setName("hokyun");
+
+        Member member2 = new Member();
+        member2.setName("hokyun");
+
+        //when
+        memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 이름 입니다");
+
+        //try catch 문으로 member2 memberservice join에 정의해 놓은 예외를 받아주는 방법
+/*
+
+        try {
+            memberService.join(member2);
+            fail();
+        } catch (IllegalStateException e) {
+            assertThat(e.getMessage()).isEqualTo("이미 존재하는 이름 입니다");
+        }
+*/
+
+        //then
+
+    }
+}
